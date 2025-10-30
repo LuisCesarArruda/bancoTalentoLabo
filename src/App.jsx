2e3a032291ac64dcc64c1432253a87c400bfa31e
LUIS CESAR ALBUQUERQUE ARRUDA <730552089@unifor.local>
2025-10-29 21:54:20 -0300

    fix: fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import { useEffect } from 'react';
 import TalentBank from './pages/TalentBank';
 
 
-// Importa a função de teste
-import { testFetchTalents } from './services/testGoogleSheetsServices';
-
 function App() {
-  useEffect(() => {
-    // Executa o teste quando o app carrega
-    testFetchTalents();
-  }, []);
+
 
   return <TalentBank />;
 }
